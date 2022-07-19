78e2dc5ddf7a9cb88ee6851311b18ef86b954a40
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-07-19 16:15:18 -0400

    Added remote feed loader and local feed loader

--- Planning/ViewController.swift
+++ Planning/ViewController.swift
@@ -7,6 +7,40 @@
 
 import UIKit
 
+protocol FeedLoader {
+    func loadFeed(completion: @escaping ([String]) -> Void)
+}
+
+class FeedViewController: UIViewController {
+    var loader: FeedLoader!
+    
+    convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loader.loadFeed { loadedItems in
+            // update UI
+        }
+    }
+}
+
+class RemoteFeedLoader: FeedLoader {
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        // do something
+    }
+}
+
+class LocalFeedLoader: FeedLoader {
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        // do something
+    }
+}
+
+
 class ViewController: UIViewController {
 //testing again
     override func viewDidLoad() {
