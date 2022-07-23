56a23c4f65f53e41ec1110b437a0b50ca5e70fdc
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-07-23 16:27:06 -0400

    Create Feed Item module

---
+++ Planning/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  Planning
+//
+//  Created by Zoufishan Mehdi on 7/23/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
