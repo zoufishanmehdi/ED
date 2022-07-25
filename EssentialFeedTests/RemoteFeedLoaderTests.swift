772585a389d0a35fb423b492eb0b75a90d17543d
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-07-25 10:55:24 -0400

    Feed Loader does not request data upon completion

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,27 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  Planning
+//
+//  Created by Zoufishan Mehdi on 7/25/22.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+ 
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+    
+}
