0c8ea4626b8c537bca0b26f68b725a89a6d1b2ec
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-07-25 12:52:17 -0400

    Move RemoteFeedLoader component to production

---
+++ Planning/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  Planning
+//
+//  Created by Zoufishan Mehdi on 7/25/22.
+//
+
+import Foundation
+
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class RemoteFeedLoader {
+    let client: HTTPClient
+    let url: URL
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: url)
+    }
+}
