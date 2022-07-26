09e04576c44cd5d124a9373571595f92634b1ba1
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-07-26 18:45:59 -0400

    Delivers empty items array on 200 HTTP response with JSON empy list

--- Planning/Feed API/RemoteFeedLoader.swift
+++ Planning/Feed API/RemoteFeedLoader.swift
@@ -38,8 +38,12 @@ public final class RemoteFeedLoader {
     public func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { result in
             switch result {
-            case .success:
+            case let .success(data, _):
+                if let _ = try? JSONSerialization.jsonObject(with: data) {
                 completion(.failure(.invalidData))
+                } else {
+                    completion(.failure(.invalidData))
+                }
             case .failure:
                 completion(.failure(.connectivity))
             }
--- PlanningTests/RemoteFeedLoaderTests.swift
+++ PlanningTests/RemoteFeedLoaderTests.swift
@@ -64,6 +64,18 @@ class RemoteFeedLoaderTests: XCTestCase {
         })
     }
     
+    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
+        
+        let (sut, client) = makeSUT()
+        
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut.load { capturedResults.append($0) }
+        
+        let emptyListJSON = Data(bytes: "{\"items\": []}".utf8)
+        client.complete(withStatusCode: 200, data: emptyListJSON)
+        
+        XCTAssertEqual(capturedResults, [.success([])])
+    }
     
     
     // MARK: - Helpers
