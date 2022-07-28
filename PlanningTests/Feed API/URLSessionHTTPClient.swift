7a2403c19b06828d4c6a1fbef00c1dc8a9bd2e29
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-07-28 14:57:39 -0400

    Creates URLSession data task with URL on URLSessionHTTPClient.get(from:URL)

---
+++ PlanningTests/Feed API/URLSessionHTTPClient.swift
@@ -0,0 +1,68 @@
+//
+//  URLSessionHTTPClient.swift
+//  Planning
+//
+//  Created by Zoufishan Mehdi on 7/28/22.
+//
+
+import XCTest
+
+class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) {_, _, _ in }
+    }
+}
+
+class URLSessionHTTPClientTests: XCTestCase {
+    
+    func test_getFromURL_createsDataTaskWithURL() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+    
+    // MARK: - Helpers
+    
+    private class URLSessionSpy: URLSession {
+        var receivedURLs = [URL]()
+        
+        override func dataTask(with url: URL, completionHandler: @escaping(Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            return FakeURLSessionDataTask()
+        }
+    }
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask {}
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
