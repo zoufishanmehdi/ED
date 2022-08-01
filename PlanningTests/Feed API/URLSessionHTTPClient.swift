80a81c9811453d98cd2b8027c4d1cb8747f6c239
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-08-01 13:00:59 -0400

    Move start/stop intercepting requests to setUp/tearDown methods

--- PlanningTests/Feed API/URLSessionHTTPClient.swift
+++ PlanningTests/Feed API/URLSessionHTTPClient.swift
@@ -26,6 +26,16 @@ class URLSessionHTTPClient {
 
 class URLSessionHTTPClientTests: XCTestCase {
     
+    override func setUp() {
+        super.setUp()
+        URLProtocolStub.startInterceptingRequests()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        URLProtocolStub.stopInterceptingRequests()
+    }
+    
     func test_getFromURL_performsGETRequestWithURL() {
         URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "http://any-url.com")!
@@ -38,7 +48,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         URLSessionHTTPClient().get(from: url) { _ in }
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInterceptingRequests()
     }
     
     func test_getFromURL_failsOnRequestError() {
@@ -63,7 +72,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInterceptingRequests()
     }
 
     // MARK: - Helpers
