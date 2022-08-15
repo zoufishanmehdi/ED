c9c068bde361358d5912e165fd931404b1cf9ef2
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-08-15 11:27:59 -0400

    Move FeedStore to its own file

---
+++ Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeedTests
+//
+//  Created by Zoufishan Mehdi on 8/15/22.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
