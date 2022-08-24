7134c6027c3726a4a340f723f74b24e29fdc66cc
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-08-24 14:34:58 -0400

    Load command requests cache retrieval

--- Planning/Feed Cache/FeedStore.swift
+++ Planning/Feed Cache/FeedStore.swift
@@ -13,5 +13,6 @@ public protocol FeedStore {
 
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+    func retrieve()
 
 }
