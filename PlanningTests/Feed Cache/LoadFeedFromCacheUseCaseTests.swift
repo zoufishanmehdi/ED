7bb47fbc2eec4b430233c1caf64d8230625ac28f
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-08-24 11:56:36 -0400

    reorganized file and folder structure

--- PlanningTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ PlanningTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -1,21 +1,21 @@
 //
 //  LoadFeedFromCacheUseCaseTests.swift
-//  Planning
+//  PlanningTests
 //
 //  Created by Zoufishan Mehdi on 8/24/22.
 //
 
 import XCTest
-import EssentialFeed
+import Planning
 
 class LoadFeedFromCacheUseCaseTests: XCTestCase {
-
+    
     func test_init_doesNotMessageStoreUponCreation() {
         let (_, store) = makeSUT()
-
+        
         XCTAssertEqual(store.receivedMessages, [])
     }
-
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
@@ -25,40 +25,40 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
     }
-
+    
     private class FeedStoreSpy: FeedStore {
         enum ReceivedMessage: Equatable {
             case deleteCachedFeed
             case insert([LocalFeedImage], Date)
         }
-
+        
         private(set) var receivedMessages = [ReceivedMessage]()
-
+        
         private var deletionCompletions = [DeletionCompletion]()
         private var insertionCompletions = [InsertionCompletion]()
-
+        
         func deleteCachedFeed(completion: @escaping DeletionCompletion) {
             deletionCompletions.append(completion)
             receivedMessages.append(.deleteCachedFeed)
         }
-
+        
         func completeDeletion(with error: Error, at index: Int = 0) {
             deletionCompletions[index](error)
         }
-
+        
         func completeDeletionSuccessfully(at index: Int = 0) {
             deletionCompletions[index](nil)
         }
-
+        
         func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
             insertionCompletions.append(completion)
             receivedMessages.append(.insert(feed, timestamp))
         }
-
+        
         func completeInsertion(with error: Error, at index: Int = 0) {
             insertionCompletions[index](error)
         }
-
+        
         func completeInsertionSuccessfully(at index: Int = 0) {
             insertionCompletions[index](nil)
         }
