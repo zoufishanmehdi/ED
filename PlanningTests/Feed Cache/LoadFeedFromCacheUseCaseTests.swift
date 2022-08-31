9d8b1935428cea0cf997c98a1600acd9b24ad1a8
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-08-31 16:08:11 -0400

    Load command does not delete less than seven days old cache

--- PlanningTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ PlanningTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -72,18 +72,30 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
                 store.completeRetrieval(with: feed.local, timestamp: moreThanSevenDaysOldTimestamp)
             })
         }
-             
+               
                func test_load_deletesCacheOnRetrievalError() {
             let (sut, store) = makeSUT()
             sut.load { _ in }
             store.completeRetrieval(with: anyNSError())
             XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
         }
-            
+               
                func test_load_doesNotDeleteCacheOnEmptyCache() {
             let (sut, store) = makeSUT()
             sut.load { _ in }
             store.completeRetrievalWithEmptyCache()
+            XCTAssertEqual(store.receivedMessages, [.retrieve])
+        }
+               
+               func test_load_doesNotDeleteCacheOnLessThanSevenDaysOldCache() {
+            let feed = uniqueImageFeed()
+            let fixedCurrentDate = Date()
+            let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+            let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+            
+            sut.load { _ in }
+            store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
+            
             XCTAssertEqual(store.receivedMessages, [.retrieve])
         }
                
