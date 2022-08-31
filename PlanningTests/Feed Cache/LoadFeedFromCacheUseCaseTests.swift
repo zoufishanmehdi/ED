980cc330a1a0297c2d4ed77fedba8b36ad5ad190
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-08-31 15:34:45 -0400

    Load command delivers no iamges on more than seven days old cache

--- PlanningTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ PlanningTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -62,64 +62,75 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         }
                }
                
-    // MARK: - Helpers
-    
-    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
-        let store = FeedStoreSpy()
-        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
-        trackForMemoryLeaks(store, file: file, line: line)
-        trackForMemoryLeaks(sut, file: file, line: line)
-        return (sut, store)
-    }
-    
-    private func expect(_ sut: LocalFeedLoader, toCompleteWith expectedResult: LocalFeedLoader.LoadResult, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
-        let exp = expectation(description: "Wait for load completion")
-        
-        sut.load { receivedResult in
-            switch (receivedResult, expectedResult) {
-            case let (.success(receivedImages), .success(expectedImages)):
-                XCTAssertEqual(receivedImages, expectedImages, file: file, line: line)
+               func test_load_deliversNoImagesOnMoreThanSevenDaysOldCache() {
+            let feed = uniqueImageFeed()
+            let fixedCurrentDate = Date()
+            let moreThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: -1)
+            let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+            
+            expect(sut, toCompleteWith: .success([]), when: {
+                store.completeRetrieval(with: feed.local, timestamp: moreThanSevenDaysOldTimestamp)
+            })
+        }
+               
+               // MARK: - Helpers
+               
+               private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+            let store = FeedStoreSpy()
+            let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+            trackForMemoryLeaks(store, file: file, line: line)
+            trackForMemoryLeaks(sut, file: file, line: line)
+            return (sut, store)
+        }
+               
+               private func expect(_ sut: LocalFeedLoader, toCompleteWith expectedResult: LocalFeedLoader.LoadResult, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+            let exp = expectation(description: "Wait for load completion")
+            
+            sut.load { receivedResult in
+                switch (receivedResult, expectedResult) {
+                case let (.success(receivedImages), .success(expectedImages)):
+                    XCTAssertEqual(receivedImages, expectedImages, file: file, line: line)
+                    
+                case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                    XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+                    
+                default:
+                    XCTFail("Expected result \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+                }
                 
-            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
-                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
-                
-            default:
-                XCTFail("Expected result \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+                exp.fulfill()
             }
             
-            exp.fulfill()
+            action()
+            wait(for: [exp], timeout: 1.0)
         }
-        
-        action()
-        wait(for: [exp], timeout: 1.0)
-    }
-    
-    private func uniqueImage() -> FeedImage {
-        return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
-    }
-    
-    private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
-        let models = [uniqueImage(), uniqueImage()]
-        let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
-        return (models, local)
-    }
-    
-    private func anyURL() -> URL {
-        return URL(string: "http://any-url.com")!
-    }
-    
-    private func anyNSError() -> NSError {
-        return NSError(domain: "any error", code: 0)
-    }
-    
-}
-
-private extension Date {
-    func adding(days: Int) -> Date {
-        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
-    }
-    
-    func adding(seconds: TimeInterval) -> Date {
-        return self + seconds
-    }
-}
+               
+               private func uniqueImage() -> FeedImage {
+            return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+        }
+               
+               private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+            let models = [uniqueImage(), uniqueImage()]
+            let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+            return (models, local)
+        }
+               
+               private func anyURL() -> URL {
+            return URL(string: "http://any-url.com")!
+        }
+               
+               private func anyNSError() -> NSError {
+            return NSError(domain: "any error", code: 0)
+        }
+               
+               }
+               
+               private extension Date {
+            func adding(days: Int) -> Date {
+                return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+            }
+            
+            func adding(seconds: TimeInterval) -> Date {
+                return self + seconds
+            }
+        }
