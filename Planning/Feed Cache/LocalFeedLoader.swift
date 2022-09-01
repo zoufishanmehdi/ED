e9c96f6ebd016d10db51c9a0b469aaec3de0521e
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-09-01 15:16:56 -0400

    Extract cache deletion side-effect on retrieval error from load method to the 'validateCache' method

--- Planning/Feed Cache/LocalFeedLoader.swift
+++ Planning/Feed Cache/LocalFeedLoader.swift
@@ -25,7 +25,6 @@ public final class LocalFeedLoader {
             guard let self = self else { return }
             switch result {
             case let .failure(error):
-                self.store.deleteCachedFeed { _ in }
                 completion(.failure(error))
                 
             case let .found(feed, timestamp) where self.validate(timestamp):
@@ -39,6 +38,11 @@ public final class LocalFeedLoader {
                 completion(.success([]))
             }
         }
+    }
+    
+    public func validateCache() {
+        store.retrieve { _ in }
+        store.deleteCachedFeed { _ in }
     }
     
     private var maxCacheAgeInDays: Int {
--- PlanningTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ PlanningTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -11,19 +11,32 @@ import XCTest
 class ValidateFeedCacheUseCaseTests: XCTestCase {
     
     func test_init_doesNotMessageStoreUponCreation() {
-            let (_, store) = makeSUT()
-
-            XCTAssertEqual(store.receivedMessages, [])
-        }
-
-        // MARK: - Helpers
+        let (_, store) = makeSUT()
         
-        private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
-            let store = FeedStoreSpy()
-            let sut = LocalFeedLoader(store: store, currentDate: currentDate)
-            trackForMemoryLeaks(store, file: file, line: line)
-            trackForMemoryLeaks(sut, file: file, line: line)
-            return (sut, store)
-        }
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    func test_validateCache_deletesCacheOnRetrievalError() {
+        let (sut, store) = makeSUT()
+        
+        sut.validateCache()
+        store.completeRetrieval(with: anyNSError())
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 0)
+    }
     
 }
