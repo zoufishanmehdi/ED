ac9996f04edf59d80b4758239d97377ebe367e8f
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-09-09 17:41:54 -0400

    Retrieving from empty cache twice delivers same empty result (no side-effects)

--- Planning/Feed Cache/CodableFeedStoreTests.swift
+++ Planning/Feed Cache/CodableFeedStoreTests.swift
@@ -15,24 +15,45 @@ class CodableFeedStore {
 }
 
 class CodableFeedStoreTests: XCTestCase {
-
+    
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = CodableFeedStore()
         let exp = expectation(description: "Wait for cache retrieval")
-
+        
         sut.retrieve { result in
             switch result {
             case .empty:
                 break
-
+                
             default:
                 XCTFail("Expected empty result, got \(result) instead")
             }
-
+            
             exp.fulfill()
         }
-
+        
         wait(for: [exp], timeout: 1.0)
     }
-
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = CodableFeedStore()
+        let exp = expectation(description: "Wait for cache retrieval")
+        
+        sut.retrieve { firstResult in
+            sut.retrieve { secondResult in
+                switch (firstResult, secondResult) {
+                case (.empty, .empty):
+                    break
+                    
+                default:
+                    XCTFail("Expected retrieving twice from empty cache to deliver same empty result, got \(firstResult) and \(secondResult) instead")
+                }
+                
+                exp.fulfill()
+            }
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
 }
