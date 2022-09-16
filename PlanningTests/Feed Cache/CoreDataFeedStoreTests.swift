ff16b2194472a292542f61d936e5c32d10fbbcaf
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-09-16 14:57:49 -0400

    Load persistent container stores upon CoreDataFeedStore initialization

--- PlanningTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ PlanningTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -15,57 +15,58 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
         
         assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
-
+    
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-
+        
     }
-
+    
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
-
+        
     }
-
+    
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
-
+        
     }
-
+    
     func test_insert_deliversNoErrorOnEmptyCache() {
-
+        
     }
-
+    
     func test_insert_deliversNoErrorOnNonEmptyCache() {
-
+        
     }
-
+    
     func test_insert_overridesPreviouslyInsertedCacheValues() {
-
+        
     }
-
+    
     func test_delete_deliversNoErrorOnEmptyCache() {
-
+        
     }
-
+    
     func test_delete_hasNoSideEffectsOnEmptyCache() {
-
+        
     }
-
+    
     func test_delete_deliversNoErrorOnNonEmptyCache() {
-
+        
     }
-
+    
     func test_delete_emptiesPreviouslyInsertedCache() {
-
+        
     }
-
+    
     func test_storeSideEffects_runSerially() {
-
+        
     }
     
     // - MARK: Helpers
-        
-        private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
-            let sut = CoreDataFeedStore()
-            trackForMemoryLeaks(sut, file: file, line: line)
-            return sut
-        }
-
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let sut = try! CoreDataFeedStore(bundle: storeBundle)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
 }
