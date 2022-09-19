81104cc0cb84e10446fa36663be30e684f81ec66
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-09-19 15:10:05 -0400

    Proved that CoreDataFeedStore side-effects run serially

--- PlanningTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ PlanningTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -69,7 +69,8 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_storeSideEffects_runSerially() {
-        
+        let sut = makeSUT()
+        assertThatSideEffectsRunSerially(on: sut)
     }
     
     // - MARK: Helpers
