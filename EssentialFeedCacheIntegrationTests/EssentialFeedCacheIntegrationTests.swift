247bd1c2df6aecb713240ed1af5d4a7a3bb3ddc8
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-09-21 10:02:26 -0400

    Clean up & undo all cache side-effects on setUp and tearDown to avoid sharing state between tests

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -9,25 +9,37 @@ import XCTest
 import Planning
 
 class EssentialFeedCacheIntegrationTests: XCTestCase {
-
+    
+    override func setUp() {
+        super.setUp()
+        
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        undoStoreSideEffects()
+    }
+    
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
-
+        
         let exp = expectation(description: "Wait for load completion")
         sut.load { result in
             switch result {
             case let .success(imageFeed):
                 XCTAssertEqual(imageFeed, [], "Expected empty feed")
-
+                
             case let .failure(error):
                 XCTFail("Expected successful feed result, got \(error) instead")
             }
-
+            
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
     }
-
+    
     // MARK: Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
@@ -39,13 +51,25 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
-
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
+    
     private func testSpecificStoreURL() -> URL {
         return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
     }
-
+    
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
     }
-
+    
 }
