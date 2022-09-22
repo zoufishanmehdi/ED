657aa8aba860eead34535636c09d1a8253613212
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-09-22 12:49:25 -0400

    Add typealias for FeedStore.DeletionResult and FeedStore.InsertionResult

--- Planning/Feed Cache/FeedStore.swift
+++ Planning/Feed Cache/FeedStore.swift
@@ -10,12 +10,15 @@ import Foundation
 public typealias CachedFeed = (feed: [LocalFeedImage], timestamp: Date)
 
 public protocol FeedStore {
-    typealias DeletionCompletion = (Error?) -> Void
-    typealias InsertionCompletion = (Error?) -> Void
+    typealias DeletionResult = Error?
+    typealias DeletionCompletion = (DeletionResult) -> Void
+    
+    typealias InsertionResult = Error?
+    typealias InsertionCompletion = (InsertionResult) -> Void
     
     typealias RetrievalResult = Result<CachedFeed?, Error>
     typealias RetrievalCompletion = (RetrievalResult) -> Void
-
+    
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch to appropriate threads, if needed.
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
@@ -27,5 +30,5 @@ public protocol FeedStore {
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch to appropriate threads, if needed.
     func retrieve(completion: @escaping RetrievalCompletion)
-
+    
 }
