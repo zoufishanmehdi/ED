1d6ceef1a31081530e3fe135f02a463db5ad04b7
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-09-29 16:56:29 -0400

    Extract system under test (sut) creation to a factory method

--- Planning/Planning/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ Planning/Planning/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -27,21 +27,27 @@ final class FeedViewController: UIViewController {
 final class FeedViewControllerTests: XCTestCase {
     
     func test_init_doesNotLoadFeed() {
-        let load = LoaderSpy()
-        _ = FeedViewController(loader: loader)
-        XCTAssertEqual(loader?.loadCallCount, 1)
+        let (_, loader) = makeSUT()
+        
+        XCTAssertEqual(loader?.loadCallCount, 0)
     }
     
     func test_viewDidLoad_loadsFeed() {
-        let loader = LoaderSpy()
-        let sut = FeedViewController(loader: loader)
-        
+        let (sut, loader) = makeSUT()
         sut.loadViewIfNeeded()
         
         XCTAssertEqual(loader.loadCallCount, 1)
     }
     
     // MARK: Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        trackForMemoryLeaks(loader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, loader)
+    }
     
     class LoaderSpy: FeedLoader {
         private(set) var loadCallCount: Int = 0
