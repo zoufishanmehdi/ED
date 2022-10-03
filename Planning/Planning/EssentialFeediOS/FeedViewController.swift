7b6b1b358894f865eb9bfccf984dae3230e70f63
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-10-03 17:02:29 -0400

    Load image URL when image view is visible

--- Planning/Planning/EssentialFeediOS/FeedViewController.swift
+++ Planning/Planning/EssentialFeediOS/FeedViewController.swift
@@ -8,16 +8,22 @@
 import UIKit
 import Planning
 
-class FeedViewController: UITableViewController {
-    private var loader: FeedLoader?
+public protocol FeedImageDataLoader {
+    func loadImageData(from url: URL)
+}
+
+final public class FeedViewController: UITableViewController {
+    private var feedLoader: FeedLoader?
+    private var imageLoader: FeedImageDataLoader?
     private var tableModel = [FeedImage]()
     
-    public convenience init(loader: FeedLoader) {
+    public convenience init(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) {
         self.init()
-        self.loader = loader
+        self.feedLoader = feedLoader
+        self.imageLoader = imageLoader
     }
     
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
         
         refreshControl = UIRefreshControl()
@@ -28,7 +34,7 @@ class FeedViewController: UITableViewController {
     
     @objc private func load() {
         refreshControl?.beginRefreshing()
-        loader?.load { [weak self] result in
+        feedLoader?.load { [weak self] result in
             if let feed = try? result.get() {
                 self?.tableModel = feed
                 self?.tableView.reloadData()
@@ -47,6 +53,7 @@ class FeedViewController: UITableViewController {
         cell.locationContainer.isHidden = (cellModel.location == nil)
         cell.locationLabel.text = cellModel.location
         cell.descriptionLabel.text = cellModel.description
+        imageLoader?.loadImageData(from: cellModel.url)
         return cell
     }
 }
