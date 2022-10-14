6fa908a4a996dc0e591f46df50340f024f711db2
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-10-14 17:34:22 -0400

    Move from property injection to constructor injection in the  to guarantee its instances have access to its dependencies at all times.

--- Planning/Planning/EssentialFeediOS/Feed UI/Feed Presentation/FeedPresenter.swift
+++ Planning/Planning/EssentialFeediOS/Feed UI/Feed Presentation/FeedPresenter.swift
@@ -24,20 +24,24 @@ protocol FeedView {
 }
 
 final class FeedPresenter {
+    private let feedView: FeedView
+    private let loadingView: FeedLoadingView
     
-    var feedView: FeedView?
-    var loadingView: FeedLoadingView?
+    init(feedView: FeedView, loadingView: FeedLoadingView) {
+        self.feedView = feedView
+        self.loadingView = loadingView
+    }
     
     func didStartLoadingFeed() {
-        loadingView?.display(FeedLoadingViewModel(isLoading: true))
+        loadingView.display(FeedLoadingViewModel(isLoading: true))
     }
     
     func didFinishLoadingFeed(with feed: [FeedImage]) {
-        feedView?.display(FeedViewModel(feed: feed))
-        loadingView?.display(FeedLoadingViewModel(isLoading: false))
+        feedView.display(FeedViewModel(feed: feed))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
         
     func didFinishLoadingFeed(with error: Error) {
-        loadingView?.display(FeedLoadingViewModel(isLoading: false))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
 }
