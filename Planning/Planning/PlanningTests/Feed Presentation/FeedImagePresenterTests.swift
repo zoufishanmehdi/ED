1e2acc66f89158fa6de787165d1ca9345187eb07
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-10-24 18:58:19 -0400

    Add fail image transformation helper to clarify intent in test setup

--- Planning/Planning/PlanningTests/Feed Presentation/FeedImagePresenterTests.swift
+++ Planning/Planning/PlanningTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -69,7 +69,7 @@ class FeedImagePresenterTests: XCTestCase {
     }
     
     func test_didFinishLoadingImageData_displaysRetryOnFailedImageTransformation() {
-        let (sut, view) = makeSUT(imageTransformer: { _ in nil })
+        let (sut, view) = makeSUT(imageTransformer: fail)
         let image = uniqueImage()
         let data = Data()
         
@@ -92,6 +92,10 @@ class FeedImagePresenterTests: XCTestCase {
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
+    }
+    
+    private var fail: (Data) -> Any? {
+        return { _ in nil }
     }
     
     private class ViewSpy: FeedImageView {
