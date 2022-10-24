39ca6a74148205cdb014ba3db5f5b9ff530f569f
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-10-24 16:50:41 -0400

    Move FeedLoadingViewModel to separate file

---
+++ Planning/Planning/Planning/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  Planning
+//
+//  Created by Zoufishan Mehdi on 10/24/22.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
