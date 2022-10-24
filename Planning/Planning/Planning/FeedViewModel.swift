adfbc2c0563b97c5452d6ee11e6e0d06074d34e7
Zoufishan Mehdi <zzmehdi@gmail.com>
2022-10-24 16:49:42 -0400

    Move FeedViewModel to separate file

---
+++ Planning/Planning/Planning/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  Planning
+//
+//  Created by Zoufishan Mehdi on 10/24/22.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
